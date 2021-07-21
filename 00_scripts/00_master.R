eb1701d686f16efc28cde01cc4f1d59d3a11d713
branmora <86805379+analistaup29@users.noreply.github.com>
2021-07-21 17:58:45 -0500

    update master

--- 00_scripts/00_master.R
+++ 00_scripts/00_master.R
@@ -4,20 +4,18 @@
 # Seleccionar acciones ---------------------------------------------------------------------
 
 # Limpieza de datos
-clean_r             <- 1  # Corre script de limpieza de datos en R
-clean_stata         <- 1  # Corre script de limpieza de datos en Stata
-clean_python        <- 1  # Corre script de limpieza de datos en Python
+limpiar_r             <- 1  # Corre script de limpieza de datos en R
+limpiar_stata         <- 1  # Corre script de limpieza de datos en Stata
+limpiar_python        <- 1  # Corre script de limpieza de datos en Python
 
 # Construccion de datos
-construct_dataset   <- 0  # Genera data construida
+construir_bases_integradas   <- 0  # Genera data construida
 
 # Rutas de Usuario ---------------------------------------------------------------------
 
 # PC Minedu Santiago
 if (Sys.getenv("USERNAME") == "analistaup18") {
-  github      <- file.path("C:/Users/ANALISTAUP18/Documents/GitHub/data_upp",
-                           fsep = .Platform$file.sep)
-  onedrive    <- file.path("B:/OneDrive - Ministerio de Educación/unidad_B/0. Data",
+  data        <- file.path("B:/OneDrive - Ministerio de Educación/unidad_B/0. Data",
                            fsep = .Platform$file.sep)
   ruta_stata  <- file.path("D:/Stata15/StataSE-64",
                            fsep = .Platform$file.sep)
@@ -60,36 +58,36 @@ options("RStata.StataVersion"  = 15)
 
 # Globales ---------------------------------------------------------------------
 
-data_raw          <- file.path(onedrive, "01_data_raw")
-data_clean        <- file.path(onedrive, "02_data_clean")
-data_constructed  <- file.path(onedrive, "03_data_constructed")
-scripts           <- file.path(github, "scripts")
+scripts               <- file.path(data, "00_scripts")
+bases_crudas          <- file.path(data, "01_bases_crudas")
+bases_limpias         <- file.path(data, "02_bases_limpias")
+bases_integradas      <- file.path(data, "03_bases_integradas")
 
 # Correr codigo ------------------------------------------------------------------
 
 # Limpiar datos de codigo R
-if (clean_r) source(file.path(scripts, "01_clean", "00_run_r.R"))
+if (limpiar_r) source(file.path(scripts, "01_clean", "00_run_r.R"))
 # INPUTS
 #  file.path(data_source1, "raw","") 
 # OUTPUTS
 #  file.path(data_source1, "clean", "")
 
 # Limpiar datos de codigo en Stata
-if (clean_stata) stata(file.path(scripts, "01_clean", "00_run_stata.do"))
+if (limpiar_stata) stata(file.path(scripts, "01_clean", "00_run_stata.do"))
 # INPUTS
 #  file.path(data_source2, "raw","") #
 # OUTPUTS 
 #  file.path(data_source2, "clean", "") #
 
 # Limpiar datos de codigo en Python
-if (clean_python) py_run_file(file.path(scripts, "01_clean", "00_run_python.py"))
+if (limpiar_python) py_run_file(file.path(scripts, "01_clean", "00_run_python.py"))
 # INPUTS
 #  file.path(data_source3, "raw","") 
 # OUTPUTS 
 #  file.path(data_source3, "clean", "") 
 
 # Construct variables and analysis dataset
-if (construct_dataset) source(file.path(scripts, "02_construct", "01_construct_baseintegrada.R"))
+if (construir_bases_integradas) source(file.path(scripts, "02_construct", "01_construct_baseintegrada.R"))
 # INPUTS 
 #  file.path(data_source1, "clean", "") 
 # OUTPUTS
